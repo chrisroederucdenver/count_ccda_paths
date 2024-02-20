6654fa9078f624b8f0389406f7b6d2feb2de5817
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-02-20 09:25:19 -0700

    added TODO notes

--- src/main/java/org/tislab/count_paths/CountPaths.java
+++ src/main/java/org/tislab/count_paths/CountPaths.java
@@ -7,6 +7,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.function.Function;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -32,6 +35,30 @@ public class CountPaths {
 		    }
 		    System.out.println("....that's all folks!");		    
 	}
+	
+	private static Map<String, Integer> leafCounts = new HashMap<String, Integer>();
+
+	// TODO  #1: how to specify the hash creation function as a lambda to make it easy to swithc them around without using interfaces etc.
+//	static Function(String a, Integer b) makeKey =  (String a) -> { return a.hashCode(); };
+	
+	// TODO #2 is what do you want to count? Just creating a hash of the entire path string will be too specific: everything will have a count of 1
+	// If you want to get some measure of genericness, but count variables per person you'll need to be able to
+	// access the different parts of the path.
+	
+	// at the risk of re-creating something from XPath (TODO: look htat up!)
+	// here's a more detailed or abstract path element class. Stack these like the strings the code
+	// below creates so you can pull apart the data chunks you want to build  key from, at the
+	// level of detail you want to count.
+	private static class PathElement {
+		String elementName;
+		Map<String, String> attributeMap = new HashMap<String, String>();
+		
+	}
+	
+	// The real hard part is deciding what values you want to include and which you don't
+	// - people, encounters, dates, variable?
+
+	
 	
 	enum Direction  { UP, DOWN };
 	
