9c4a600c91f9e610292f2d7d85af9ddaa68dc471
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-03-05 15:28:25 -0700

    added test data, turned append off for the output files

--- src/main/java/org/tislab/count_paths/CountPaths.java
+++ src/main/java/org/tislab/count_paths/CountPaths.java
@@ -1,15 +1,17 @@
 package org.tislab.count_paths;
 
-
-
 import java.util.List;
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.function.Function;
+import java.util.Arrays;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -19,27 +21,42 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-
-
 public class CountPaths {
 
 	public static void main(String[] args) {
+				
 	    SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-		    try {
-		        SAXParser saxParser = saxParserFactory.newSAXParser();
-		        MyHandler handler = new MyHandler();
-		        //saxParser.parse(new File("src/main/resources/Inpatient_Encounter_Discharged_to_Rehab_Location(C-CDA2.1).xml"), handler);
-		        saxParser.parse(new File("src/main/resources/test_2.xml"), handler);		        
-		    } catch (ParserConfigurationException | SAXException | IOException e) {
-		        e.printStackTrace();
-		    }
-		    System.out.println("....that's all folks!");		    
+	    try {
+	        SAXParser saxParser = saxParserFactory.newSAXParser();
+	        MyHandler handler = new MyHandler();
+	        
+	        for (String filename : file_list) {
+	        	String out_filename = filename + ".out";
+	        	out_stream = new PrintStream(new FileOutputStream(out_filename, false));
+	        	System.out.println("==== " + filename + " =======================");
+	        	saxParser.parse(new File("src/main/resources/" + filename), handler);
+
+	        }
+	    } catch (ParserConfigurationException | SAXException | IOException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    System.out.println("....that's all folks!");		    
 	}
 	
+	private static boolean show_attributes = true;
+	private static PrintStream out_stream = System.out;
 	private static Map<String, Integer> leafCounts = new HashMap<String, Integer>();
+	private static final List<String> file_list = Arrays.asList(
+			"test_2.xml",
+			"170.314b2_AmbulatoryToC.xml", 
+			"CCDA_CCD_b1_Ambulatory_v2.xml", 
+			"CCDA_CCD_b1_InPatient_v2.xml", 
+			"Inpatient_Encounter_Discharged_to_Rehab_Location(C-CDA2.1).xml", 
+			"ToC_CCDA_CCD_CompGuideSample_FullXML.xml");
 
 	// TODO  #1: how to specify the hash creation function as a lambda to make it easy to swithc them around without using interfaces etc.
-//	static Function(String a, Integer b) makeKey =  (String a) -> { return a.hashCode(); };
+    // static Function(String a, Integer b) makeKey =  (String a) -> { return a.hashCode(); };
 	
 	// TODO #2 is what do you want to count? Just creating a hash of the entire path string will be too specific: everything will have a count of 1
 	// If you want to get some measure of genericness, but count variables per person you'll need to be able to
@@ -49,10 +66,10 @@ public class CountPaths {
 	// here's a more detailed or abstract path element class. Stack these like the strings the code
 	// below creates so you can pull apart the data chunks you want to build  key from, at the
 	// level of detail you want to count.
+	
 	private static class PathElement {
 		String elementName;
 		Map<String, String> attributeMap = new HashMap<String, String>();
-		
 	}
 	
 	// The real hard part is deciding what values you want to include and which you don't
@@ -63,8 +80,11 @@ public class CountPaths {
 	enum Direction  { UP, DOWN };
 	
 	static class MyHandler extends DefaultHandler {
+		
 		Deque<String> pathStack = new ArrayDeque();
 		Direction pathDirection = Direction.DOWN;
+		int element_level = 0;
+		StringBuilder data = new StringBuilder();		
 		
 		String getPathString() {	
 			StringBuilder pathString = new StringBuilder(); 
@@ -74,8 +94,6 @@ public class CountPaths {
 			}
 			return pathString.toString();
 		}
-		
-		StringBuilder data = new StringBuilder();
 			
 		public void startDocument() 
 				throws SAXException {
@@ -87,35 +105,36 @@ public class CountPaths {
 		
 		public void startElement(String uri, String localName, String qname, Attributes attributes)
 				throws SAXException {
+			element_level++;
 			data = new StringBuilder();
 			pathDirection = Direction.DOWN;
 			StringBuilder sb = new StringBuilder();
 			sb.append(qname);	
 			if (attributes.getLength() > 0) {
-				sb.append("[");
-				for (int i=0; i<attributes.getLength(); i++) {
-					sb.append("@");
-					sb.append(attributes.getLocalName(i));
-					sb.append("='");
-					sb.append(attributes.getValue(i)); 
-					sb.append("'");				
-					if (attributes.getLength() > (i + 1)) { sb.append(" & "); }
-				}
-				sb.append("]");				
+				if (show_attributes && element_level > 1) {				
+					sb.append("[");
+					for (int i=0; i<attributes.getLength(); i++) {
+						sb.append("@");
+						sb.append(attributes.getLocalName(i));
+						sb.append("='");
+						sb.append(attributes.getValue(i)); 
+						sb.append("'");				
+						if (attributes.getLength() > (i + 1)) { sb.append(" & "); }
+					}
+					sb.append("]");
+				} 
 			}
-
-			
 			pathStack.addLast(sb.toString());
 		}
 		
 		public void endElement(String uri, String localName, String qname)
 				throws SAXException {
-
+			element_level--;
 			if (pathDirection == Direction.DOWN ) {
 				if (data.length() > 0) { 
-					System.out.println(getPathString() + "\"" + data + "\"");
+					out_stream.println(getPathString() + "\"" + data + "\"");
 				} else {
-					System.out.println(getPathString());
+					out_stream.println(getPathString());
 				}
 			}
 			pathDirection = Direction.UP;				
@@ -130,5 +149,7 @@ public class CountPaths {
 		}
 	}
 }
+
+
 
 
