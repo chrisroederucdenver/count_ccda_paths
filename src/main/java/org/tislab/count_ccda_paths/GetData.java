7e04dc48bf08c56f41f1045e7b27863d8e30915d
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-03-09 16:56:48 -0700

    progress: can now  lift values from XML text and attributes driven by XPaths

--- src/main/java/org/tislab/count_ccda_paths/GetData.java
+++ src/main/java/org/tislab/count_ccda_paths/GetData.java
@@ -12,12 +12,21 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 import org.w3c.dom.*;
+import org.w3c.dom.Document;
+
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import org.xml.sax.SAXException;
+
+//import javax.xml.validation.Schema;
+//import javax.xml.validation.SchemaFactory;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 
 
 
@@ -25,10 +34,11 @@ import org.xml.sax.SAXException;
 class GetData {
 	
 	//static final String paths_filename = "src/main/resources/PersonPaths.txt";
-	final String paths_filename = "PersonPaths.txt";
+	static String paths_filename = "PersonPaths.txt";
 	BufferedReader pathsReader=null;
+	private XPath xPath=null;
+	private Document ccdaDocument=null;
     private static final List<String> file_list = Arrays.asList(
-            "test_2.xml",
             "170.314b2_AmbulatoryToC.xml",
             "CCDA_CCD_b1_Ambulatory_v2.xml",
             "CCDA_CCD_b1_InPatient_v2.xml",
@@ -37,24 +47,31 @@ class GetData {
 	
 	
 	public static void main(String[] args) {
-		String ccda_filepath = file_list.get(5);
-		System.out.println("GET DATA: " + ccda_filepath);
-		
-		try {
-			GetData gd = new GetData(ccda_filepath);
-//			gd.processPathList();
-		}
-		catch (ParserConfigurationException pce) {
-			System.err.println("parser not correctly configured." + pce);
-			System.err.println(pce.toString());			
-		}
-		catch (IOException ioe) {
-			System.err.println("cant' read the CCDA file: " + ccda_filepath);
-			System.err.println(ioe.toString());
-		}
-		catch (SAXException se) {
-			System.err.println("can't parse the CCDA file: " + ccda_filepath);
-			System.err.println(se.toString());			
+		for (String ccda_filepath : file_list) {
+			System.out.println("GET DATA: " + ccda_filepath);
+			
+			try {
+				GetData gd = new GetData(ccda_filepath);
+				//gd.processPathList();
+				
+				paths_filename="ProviderPaths.txt";
+				gd.processPathList();
+			}
+			catch (ParserConfigurationException pce) {
+				System.err.println("parser not correctly configured." + pce);
+				System.err.println(pce.toString());			
+			}
+			catch (IOException ioe) {
+				System.err.println("cant' read the CCDA file: " + ccda_filepath);
+				System.err.println(ioe.toString());
+			}
+			catch (SAXException se) {
+				System.err.println("can't parse the CCDA file: " + ccda_filepath);
+				System.err.println(se.toString());			
+			} catch (XPathExpressionException e) {
+				System.err.println("can't use expression in the xPath the CCDA file: ");			
+				e.printStackTrace();
+			}
 		}
 				
 	}
@@ -62,20 +79,12 @@ class GetData {
 	public GetData(String ccdaFilePath) 
 	throws ParserConfigurationException, IOException, SAXException {
 		InputStream ccdaStream = getStreamFromClasspath(ccdaFilePath);
-		
 		// https://docs.oracle.com/javase/8/docs/api/javax/xml/parsers/DocumentBuilderFactory.html#newDocumentBuilder--
 		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-		
-		// Schema schema = SchemaFactory.newSchema();   // W3C_XML_SCHEMA_INSTANCE_NS_URI // CDA.xsl
-		//docBuilderFactory.setSchema()
-		//docBuilderFactory.setValidating()
-		//docBuilderFactory
-
-		
-		//showStreamContents(ccdaStream);
-		
 		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();		
-		docBuilder.parse(ccdaStream);
+		ccdaDocument = docBuilder.parse(ccdaStream);
+		
+		xPath = XPathFactory.newInstance().newXPath();
 	}
 	
 	public void showStreamContents(InputStream is) {
@@ -99,7 +108,9 @@ class GetData {
 		return is;
 	}
 	
-	public void processPathList() {
+	public void processPathList()
+	throws XPathExpressionException {
+		
 		BufferedReader pathsReader = null;
 		try {
 			pathsReader = new BufferedReader(new InputStreamReader(getStreamFromClasspath(paths_filename)));
@@ -112,20 +123,24 @@ class GetData {
 
 		try {
 			while (pathsReader.ready()) {
-				processXPath(pathsReader.readLine());
+				String s = pathsReader.readLine();
+				if (!s.equals("") && s.charAt(0) != '#') {
+					processXPath(s);
+				}
 			}
 		}
 		catch (IOException x) {
 				System.out.println("can't read " + x);
 		}
-		
-		
 	}
 	
-	public void processXPath(String xPath) {
+	public void processXPath(String xPath_expression_string) 
+	throws XPathExpressionException{
+		System.out.print("xPath expression: \"" + xPath_expression_string + "\"");
+		XPathExpression expr = xPath.compile(xPath_expression_string);
 
-		
-		
+		String nodeList = expr.evaluate(ccdaDocument); 
+		System.out.println("   value: \"" + nodeList + "\"");
 	}
 	
 	
